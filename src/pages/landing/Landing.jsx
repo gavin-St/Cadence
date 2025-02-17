586ee776d6e257594ead39e8637c800c2d256f7c
Gavin Song <gavins3math@gmail.com>
2025-02-17 01:49:16 -0500

    fix landing page

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -3,13 +3,19 @@ import React from 'react'
 import Header from "./Header";
 import Carousel from "./Carousel";
 import Transition from "./Transition";
+import githubIcon from "../../res/github-icon.png"
+import './landing.css';
 
 function Landing() {
   return (
-    <div>
+    <div className='landing'>
       <Header />
       <Carousel />
       <Transition />
+      <a href="https://github.com/gavin-St/Cadence" target='_blank' className="github-link">
+        <img src={githubIcon} alt="GitHub Link" />
+      </a>
+      <p className='github-credits'>Check out the GitHub!</p>
     </div>
   )
 }
